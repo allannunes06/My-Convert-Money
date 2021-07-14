1401f834be1b499c4601f49babbf49f971e1dfb5
Allan Nunes <allan.vieira06@gmail.com>
2021-07-14 18:26:08 -0300

    New conversor mode!

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const button = document.getElementsByTagName('button')[0]
 const select = document.getElementById("currency-select")
+const select1 = document.getElementById("selection")
 
 const dolar = 5.22
 const euro = 6.11
+const bitcoin = 0.0000060
 
 
 const convertValues = () => {
@@ -17,18 +19,61 @@ const convertValues = () => {
         currency: "BRL",
     }).format(inputReal)
 
-    if (select.value === "US$ Dólar americano") {
+    if (select1.value ==="R$ Real brasileiro" && select.value === "US$ Dólar americano") {
         currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD",
         }).format(inputReal / dolar);
     }
 
-    if (select.value === "€ Euro") {
+    if (select1.value ==="R$ Real brasileiro" && select.value === "€ Euro") {
         currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR",
         }).format(inputReal / euro);
+    }
+
+    if (select1.value ==="R$ Real brasileiro" && select.value === "₿ Bitcoin") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputReal * bitcoin);
+    }
+
+    if (select1.value ==="US$ Dólar americano" && select.value === "R$ Real brasileiro") {
+        realValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputReal)
+        
+        currencyValueText.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+        }).format(dolar * inputReal);
+    }
+
+    if (select1.value === "€ Euro" && select.value === "R$ Real brasileiro" ) {
+        realValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReal)
+        
+        currencyValueText.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+        }).format( euro * inputReal);
+    }
+
+    if (select1.value === "₿ Bitcoin" && select.value === "R$ Real brasileiro" ) {
+        realValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputReal)
+        
+        currencyValueText.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+        }).format(inputReal * bitcoin);
     }
 
 }
@@ -52,6 +97,42 @@ changeCurrency = () => {
         currencyName.innerHTML = "Euro";
         currencyImg.src = "./assets/euro.png";
     }
+
+    if (select.value === "₿ Bitcoin") {
+        currencyName.innerHTML = "Bitcoin";
+        currencyImg.src = "./assets/bitcoin.png";
+    }
+
+    if (select.value === "R$ Real brasileiro") {
+        currencyName.innerHTML = "Real";
+        currencyImg.src = "./assets/brasil.png";
+    }
+    convertValues()
+};
+
+changeCurrency1 = () => {
+    const currencyName1 = document.getElementById("currency-name1");
+    const currencyImg1 = document.getElementById("currency-img1");
+
+    if (select1.value === "US$ Dólar americano") {
+        currencyName1.innerHTML = "Dólar americano";
+        currencyImg1.src = "./assets/eua.png";
+    }
+
+    if (select1.value === "€ Euro") {
+        currencyName1.innerHTML = "Euro";
+        currencyImg1.src = "./assets/euro.png";
+    }
+
+    if (select1.value === "₿ Bitcoin") {
+        currencyName1.innerHTML = "Bitcoin";
+        currencyImg1.src = "./assets/bitcoin.png";
+    }
+
+    if (select1.value === "R$ Real brasileiro") {
+        currencyName1.innerHTML = "Real";
+        currencyImg1.src = "./assets/brasil.png";
+    }
     convertValues()
 };
 
@@ -60,3 +141,4 @@ changeCurrency = () => {
 
 button.addEventListener("click", convertValues)
 select.addEventListener("change", changeCurrency)
+select1.addEventListener("change", changeCurrency1)
